99220d88c2e8c36bf2d82ccbb4d4e587aa094512
jannekelly <50674399+jannekelly@users.noreply.github.com>
2021-08-24 18:36:05 -0300

    Viwes e init

--- afazer/blueprints/webui/__init__.py
+++ afazer/blueprints/webui/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from .views import login, listar_atividades, visualizar_menu_gestor, gerenciar_atividades, visualizar_menu_usuario, sair
+from .views import login, visualizar_menu_gestor, gerenciar_atividades, visualizar_menu_usuario, sair, editar_atividade, criar_atividade, excluir_atividade
 
 
 bp = Blueprint("webui", __name__, template_folder="templates")
@@ -10,9 +10,14 @@ bp.add_url_rule("/", view_func=login, endpoint="login")
 bp.add_url_rule(
     "/gestor", view_func=visualizar_menu_gestor, endpoint="gestorview"
 )
+visualizar_menu_usuario.methods=['GET','POST']
+editar_atividade.methods = ['GET','POST']
+criar_atividade.methods=['GET', 'POST']
+bp.add_url_rule("/gestor/criar",view_func=criar_atividade,endpoint="gestorcriar")
 bp.add_url_rule(
-    "/gestor/atividades", view_func=listar_atividades, endpoint="atividadeview"
+    "/gestor/editar/<int:id>", view_func=editar_atividade, endpoint="editarview"
 )
+bp.add_url_rule("/gestor/excluir/<int:id>",view_func=excluir_atividade,endpoint="excluirview")
 bp.add_url_rule(
     "/gestor/gerenciar_atividades", view_func=gerenciar_atividades, endpoint="gerenciaratividadesview"
 )
@@ -21,6 +26,10 @@ bp.add_url_rule(
 bp.add_url_rule(
     "/usuario/", view_func=visualizar_menu_usuario, endpoint="menu_usuarioview"
 )
+bp.add_url_rule(
+    "/usuario/editar/<int:id>", view_func=editar_atividade, endpoint="editar"
+)
+
 bp.add_url_rule(
     "/usuario/sair/", view_func=sair, endpoint="sair"
 )
--- afazer/blueprints/webui/views.py
+++ afazer/blueprints/webui/views.py
@@ -13,6 +13,8 @@ def login():
         usuario = Usuario.query.filter_by(login=email).first()
         if usuario is not None and check_password_hash(usuario.senha, senha):
             login_user(usuario, remember=remember)
+            if current_user.tipo_usuario=="gestor":
+                return redirect(url_for('webui.gestorview'))
             return redirect(url_for('webui.menu_usuarioview'))
         flash("Email ou senha incorretos, tente novemente.")
         return render_template('loginUser.html')
@@ -28,15 +30,39 @@ def sair():
 
 @login_required
 def visualizar_menu_gestor():
-    return render_template('gestor.html')
+    if request.method=='POST':
+        pass
+    atividades = Atividade.query.all()
+    print(atividades)
+    return render_template('atividades.html', atividades=atividades)
 
 
 @login_required
-def listar_atividades():
-    atividades = Atividade.query.all()
-    # response = [{'id': i.id, 'nome': i.nome, 'pessoa': i.pessoa.nome} for i in atividades]
-    return render_template('atividades.html', atividades=atividades)
+def editar_atividade(id):
+    if current_user.tipo_usuario =="gestor":
+        return render_template('editar_atividade.html',id=id)
 
+    atividades = Atividade.query.filter_by(pessoa_id=current_user.pessoa_id)
+
+    return render_template('menu_usuario.html', atividades=atividades,id=id)
+@login_required
+def excluir_atividade(id):
+    atividade=Atividade.query.filter_by(id=id).first()
+    atividade.delete()
+    return redirect(url_for('webui.gestorview'))
+@login_required
+def criar_atividade():
+    if request.method=='POST':
+        nome = request.form['nome']
+        responsavel = request.form.get('responsavel')
+        pessoa=Pessoa.query.filter_by(nome=responsavel).first()
+        atividade=Atividade(nome=nome,pessoa=pessoa,status="Por Fazer")
+        atividade.save()
+        return redirect(url_for('webui.gestorview'))
+    pessoas=Usuario.query.filter_by(tipo_usuario="usuario").all()
+    return render_template('criar_atividade.html',pessoas=pessoas)
+def buscar_atividade():
+    pass
 
 @login_required
 def gerenciar_atividades():
@@ -45,7 +71,29 @@ def gerenciar_atividades():
 
 @login_required
 def visualizar_menu_usuario():
+
     atividades = Atividade.query.filter_by(pessoa_id=current_user.pessoa_id)
+    #print(type(current_user.pessoa_id))
+    id = request.args.get('id')
+    #status=request.form.get('status',type=str)
+    print("teste aqui",id)
+    if request.method=="POST":
+        #status = request.form.get('status',type=str)
+        print("POST")
+        id=request.form.get('id',type=int)
+        status = request.form.get('status', type=str)
+        print(id,status)
+        atividade = Atividade.query.filter_by(id=id).first()
+        atividade.status=status
+       # print(atividade.status)
+        atividade.save()
+        return render_template('atividades.html', atividades=atividades)
+    elif request.method=="GET":
+        print("geet")
+        id=request.args.get('id')
+        print(id)
+        pass
+    print("methods fail")
     return render_template('atividades.html', atividades=atividades)
 
 
